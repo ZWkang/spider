dcb9881a789f9c589041b161a91accafcc4ca72d
ZWkang <907747874@qq.com>
2016-09-01 23:09:56 +0800

    修复多线程抓取  原本的实际是多线程抓取几个页面导致重复抓取

--- 采集代理ip-getagent/getagent.py
+++ 采集代理ip-getagent/getagent.py
@@ -14,96 +14,127 @@ sys.setdefaultencoding('utf-8')
 
 
 #快代理
-def kuai():
+def kuai(x):
 	global lock
-	print '快代理采集中'.decode('utf-8')
-
-	kuaidaili = ['inha','intr','outha','outtr']
+	print '快代理采集中     线程 ----'.decode('utf-8')+str(x)
+	global kuaisss_queue
+	
 	#url的列表用于拼凑
-	for x in range(len(kuaidaili)):
-		sleep(1)
-		for y in range(1,4):
-		#range决定了要读取多少页   其实一般代理都是前面一些页面比较多可用的
-			kuai_queue.put(y)
-		if kuai_queue.qsize()==0:
-			break
-		while kuai_queue.qsize()>=1:
-			url = 'http://www.kuaidaili.com/free/'+str(kuaidaili[x])+'/'+str(kuai_queue.get())
-			# print url
-			r = requests.get(url,headers=random.choice(headers))
-			if r.status_code!=200:
-				sleep(5)
-				#设置一个定时休息因为错误有时候可能是过度频繁
+	while kuaisss_url_queue.qsize()>0:
+		url = kuaisss_url_queue.get()
+		print kuaisss_url_queue.qsize()
+		sleep(2)
+		r = requests.get(url,headers=random.choice(headers))
+		print r.status_code
+		if r.status_code==503:
+			print "状态码为503 ".decode('utf-8').encode('gbk')
+			lock.acquire()
+			f=open('kuaierror.txt','a+')
+			f.write(time.strftime('%Y-%m-%d %H:%M:%S')+'    '+url+'   '+str(r.status_code)+'\n')
+			f.close()
+			lock.release()
+			kuaisss_url_queue.put(url)
+			#失败了再次压入kuai_url的队列里面等待运行
+			# kuaisss_queue
+			#设置一个定时休息因为错误有时候可能是过度频繁
+		if r.status_code==200:
 			soup =  BeautifulSoup(r.content)
-			# print r.content
-			c = soup.find('table').find_all('td')
+			try:
+				c = soup.select('#list > table')[0].find_all('td')
+				lock.acquire()
+				f=open('kuaisuccess.txt','a+')
+				f.write(time.strftime('%Y-%m-%d %H:%M:%S')+'    '+url+'   '+str(r.status_code)+'\n')
+				#写入正确日志
+				f.close()
+				lock.release()
+			except:
+				c=[]
+				#这种时候一般是状态码不为200
 			for i in range(len(c)):
 				if i%7==0:
 					dailiurl=c[i+3].get_text().lower()+'://'+c[i].get_text()+':'+c[(i+1)].get_text()
 					work_queue.put(dailiurl)
 					#这个就没写。压入队列就可以了
-					print dailiurl+' 采集成功'
+					print dailiurl+' 快采集成功'
 				else:
 					pass
-	print '采集网页代理ip结束'.decode('utf-8')
+	print '采集网页代理ip结束     线程 ---------'.decode('utf-8')+str(x)
+	# sleep(2)
 
 #西刺
-def xici():
-	print '西刺采集进行中'.decode('utf-8')
-	
-	xici = ['nn','nt','wn','wt']
+def xici(x):
+	print '西刺采集进行中      线程 ---------'.decode('utf-8')+str(x)
+	global xici_url_queue
 	global lock
-	for x in range(len(xici)):
-		sleep(1)
-		for y in range(1,4):
-			url = 'http://www.xicidaili.com/'+str(xici[x])+'/'+str(y)
-			r = requests.get(url,headers=random.choice(headers))
-			if r.status_code!=200:
-				sleep(6)
-				#设置一个定时休息因为错误有时候可能是过度频繁
+	while xici_url_queue.qsize()>0:
+		url=xici_url_queue.get()
+		# print url
+		sleep(2)
+		r = requests.get(url,headers=random.choice(headers))
+		# proxies = {""}
+		if r.status_code!=200:
+			print "状态码不为200 ".decode('utf-8').encode('gbk')
+			lock.acquire()
+			f=open('xicisuccess.txt','a+')
+			f.write(time.strftime('%Y-%m-%d %H:%M:%S')+'    '+url+'   '+str(r.status_code)+'\n')
+			f.close()
+			xici_url_queue.put(url)
+			lock.release()
+			sleep(6)
+			#设置一个定时休息因为错误有时候可能是过度频繁
+		try:
 			soup =  BeautifulSoup(r.content)
 			c = soup.select('tr > td')
-			for i in range(len(c)):
-				if i%10==1:
-					ipport=c[i+4].get_text().lower()+'://'+c[i].get_text()+':'+c[i+1].get_text()
-					work_queue.put(ipport)
-					lock.acquire()
-					files = open('xiciIp.txt','a+')
-					files.write(ipport+'\r\n')
-					files.close()
-					lock.release()
-					#其实可以不写入文件的
-					#可有可无吧
-					print ipport+' 采集成功'
-				else:
+			lock.acquire()
+			f=open('xicierror.txt','a+')
+			f.write(time.strftime('%Y-%m-%d %H:%M:%S')+'    '+url+'   '+str(r.status_code)+'\n')
+			f.close()
+			lock.release()
+		except Exception:
+			c=[]
+		for i in range(len(c)):
+			if i%10==1:
+				ipport=c[i+4].get_text().lower()+'://'+c[i].get_text()+':'+c[i+1].get_text()
+				work_queue.put(ipport)
+				lock.acquire()
+				files = open('xiciIp.txt','a+')
+				files.write(ipport+'\r\n')
+				files.close()
+				lock.release()
+				#其实可以不写入文件的
+				#可有可无吧
+				print ipport+'西刺采集成功'
+			else:
 					pass
-			# break
-	
 	print '采集xici网页代理ip结束'.decode('utf-8')
 #http://www.cz88.net/
-def getcz():
-	print "采集 http://www.cz88.net/ 开始".decode('utf-8')
+def getcz(x):
+	global cz_queue
+	print "采集 http://www.cz88.net/ 开始      线程 ---------".decode('utf-8')+str(x)
 	url = ['index','http_']
-	for i in range(1,6):
+	sleep(2)
+	while cz_queue.qsize()>0:
+		i=int(cz_queue.get())
 		if i ==1:
 			urls = 'http://www.cz88.net/proxy/'+str(url[0])+'.shtml'
 		else:
 			urls= 'http://www.cz88.net/proxy/'+url[1]+str(i)+'.shtml'
-
+		# print urls
 		print urls+'此页面采集开始'.decode('utf-8')
-		sleep(1)
+		# sleep(1)
 		r = requests.get(urls,headers=random.choice(headers))
 		if r.status_code!=200:
+			print "状态码不为200 ".decode('utf-8').encode('gbk')
 			sleep(6)
 		soup = BeautifulSoup(r.content)
 		iplen = soup.select('.ip')
 		port = soup.select('.port')
-		# print iplen[1].string
 		for i in range(1,len(iplen)):
 			# print iplen[i].string
 			ipports = 'http://'+iplen[i].string+':'+str(port[i].get_text())
 			work_queue.put(ipports)
-			print str(ipports)+' 采集成功'.decode('utf-8')
+			print str(ipports)+' cz88采集成功'.decode('utf-8')
+	print '采集cz99网页代理ip结束     线程 ---------'.decode('utf-8')+str(x)
 
 
 
@@ -111,41 +142,38 @@ def getcz():
 def isAlive(x):
 	global lock
 	threadname = str('线程-'+str(x+1))
-	# print 
-	sleep(10)
+	sleep(90)
 	print (threadname+'-开启 测试开始').decode('utf-8').encode('gbk')
-	# print work_queue.qsize()
 	while work_queue.qsize()>0:
 		url=work_queue.get()
 		xxx = url.split(':')
-		# url='http://123.124.168.149:80'
 		proxies={xxx[0]:url}
 		# print url
 		try:
+			sleep(1)
 			r = requests.get('http://1212.ip138.com/ic.asp',proxies=proxies,timeout=3)
 			if r.status_code==200:
 				print threadname+'---'+url+(' 测试成功 写入文件 '.decode('utf-8'))
 				lock.acquire()
-				files = open('kuaiOK.txt','a+')
+				files = open('TestOK.txt','a+')
 				files.write(url+'\r\n')
 				files.close()
 				lock.release()
+				#测试写入文件
 			else:
 				pass
 		except Exception,e:
 			# 
-			# print Exception
 			print threadname+'---'+url+' 测试失败 当前还有'+str(work_queue.qsize())+'待测试'.decode('utf-8')
 			lock.acquire()
-			files = open('httpbad.txt','a+')
+			files = open('Testbad.txt','a+')
 			files.write(url+' unwork'+'\r\n')
 			files.close()
 			lock.release()
 			# break
-		# print '测试结束'
-		# return False
-	return False
 
+	return False
+lock = threading.Lock()
 if __name__ == '__main__':
 	start = time.clock()
 	print 'start'
@@ -163,31 +191,61 @@ if __name__ == '__main__':
 	work_queue = Queue.Queue()
 	#测试工作队列
 	threads = []
-	global lock
-	#创建锁
-	lock = threading.Lock()
+
+	#锁是全局共享 线程也共享栈	
 	# 创建线程对象
 	caijinumber = raw_input('选择开启采集进程个数'.decode('utf-8').encode('gbk'))
 	testnumber = raw_input('选择开启测试进程个数'.decode('utf-8').encode('gbk'))
 
 	num1=int(caijinumber)
 	num2=int(testnumber)
+	
+	
+
+	#cz页面url队列生产
+	cz_queue = Queue.Queue()
+	for i in xrange(1,10):
+		cz_queue.put(i)
+	#kuai代理页面队列生产
+	kuaisss_url_queue=Queue.Queue()
+	kuaidaili = ['inha','intr','outha','outtr']
+	for i in xrange(1,180):
+		for x in xrange(len(kuaidaili)):
+			kuaisss_url_queue.put('http://www.kuaidaili.com/free/'+str(kuaidaili[x])+'/'+str(i))
+	
+	# print kuaisss_url_queue.qsize()
+	#西刺网站生产URL页面队列
+	xici_url_queue=Queue.Queue()
+	xici_types = ['nn','nt','wn','wt']
+	for i in xrange(1,100):
+		for page_type in xici_types:
+			xici_url_queue.put('http://www.xicidaili.com/'+str(page_type)+'/'+str(i))
+
+
+	# print xici_url_queue.qsize()
+
 	for x in xrange(num1):
-		# t1 = 
-		threads.append(threading.Thread(target=kuai))
-		threads.append(threading.Thread(target=xici))
-		threads.append(threading.Thread(target=getcz))
+		threads.append(threading.Thread(target=kuai,args=(x,)))
+		# 快代理
+		threads.append(threading.Thread(target=xici,args=(x,)))
+		#西刺
+		threads.append(threading.Thread(target=getcz,args=(x,)))
 		#添加采集线程
-		# sleep(5)
+
 	for x in xrange(0, num2):
-		# t1 = 
+
 		threads.append(threading.Thread(target=isAlive,args=(x,)))
 		# 添加测试线程
 		# sleep(3)
 	for t in threads:
+
 		t.setDaemon(True)
+		sleep(2)
 		t.start()
 	for t in threads:
 		t.join() 
+	for z in threads:
+		print z.getName()
+		print z.isAlive()
 	end = time.clock()
 	print 'Running time: %s Seconds'%(end-start)
